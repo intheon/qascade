5979c8d24373d0c0a99fcca7f2e0e2bf98b69b44
Nima Bigdely-Shamlo <nimabg@bitbucket.org>
2017-04-16 22:49:18 -0700

    encodes file content as Byte64 and exports qascade record as JSON.

--- python/qascade_walker.py
+++ python/qascade_walker.py
@@ -1,18 +1,22 @@
 import os
+import base64
+import json
 
 qascade_record = {'filenames': [], 'file_contents': {}}
 container_root_folder = r'C:\Users\Nima\Documents\MATLAB\tools\qascade\matlab\unit_test\test2\container'
 for root, dirs, files in os.walk(container_root_folder, topdown=False):
      for name in files:
         root_relative_to_container = root[len(container_root_folder)+1:]
+        filename_relative_to_container = os.path.join(root_relative_to_container, name)
         # print(os.path.join(root_relative_to_container, name))
-        qascade_record['filenames'].append(os.path.join(root_relative_to_container, name))
+        qascade_record['filenames'].append(filename_relative_to_container)
         full_filename = os.path.join(root, name)
         name_part, extension = os.path.splitext(name)
         if (extension in {'.tsv', '.xlsx'}) or name == 'manifest.qsc.yaml':
-            print(name)
+            # print(name)
             read_bytes = open(full_filename, 'rb').read()
-            print(read_bytes)
+            read_bytes64 = base64.b64encode(read_bytes).decode('ascii')
+            # print(read_bytes64)
+            qascade_record['file_contents'][filename_relative_to_container] = read_bytes64
 
-
-# print(qascade_record['filenames'])
+print(json.dumps(qascade_record))
