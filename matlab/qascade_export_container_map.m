d10e1b323b682388cdab5d966b24bd95b1ac53ef
Nima Bigdely-Shamlo <nima.bigdely@qusp.io>
2018-03-06 16:50:02 -0800

    added JSON and JOSNP export

--- matlab/qascade_export_container_map.m
+++ matlab/qascade_export_container_map.m
@@ -1,6 +1,13 @@
-function qascade_export_container_map(folderOrFileKeyValues, exportPath, JSONPVar)
-% function qascade_export_container_map(folderOrFileKeyValues, exportPath)
-% 
+function qascade_export_container_map(folderOrFileKeyValues, exportPath, callbackName)
+% function qascade_export_container_map(folderOrFileKeyValues, exportPath, callbackName)
+% exports Qascade file metadata for the container specified in folderOrFileKeyValues as
+% a file specified in exportPath. The type of exported data is determined by the file extension.
+% Supported export file extensions are:
+%
+% .yaml: export as YAML
+% .json: export as JSON
+% .js  : export as a JSONP with the callback specified in callbackName.
+%        if not callback name is specified 'getData' is used.
 
 if ischar(folderOrFileKeyValues)
     folderOrFileKeyValues = qascade_read(folderOrFileKeyValues);
@@ -19,9 +26,29 @@ switch ext
         exportType = 'treeview';
 end;
 
-if ~exist('JSONPVar', 'var')
-    JSONPVar = 'data';
+if ~exist('callbackName', 'var')
+    callbackName = 'getData';
 end;
 
 filekvsFullNames = nestd_cell_string_to_cell_string(folderOrFileKeyValues);
-WriteYaml(exportPath, filekvsFullNames);
+
+switch exportType
+    case 'yaml'
+        WriteYaml(exportPath, filekvsFullNames);
+    case {'json' 'jsonp'}
+        opt.ForceRootName = false;
+        opt.SingletCell = true;  % even single cells are saved as JSON arrays.
+        opt.SingletArray = false; % single numerical arrays are NOT saved as JSON arrays.
+        json_str = savejson_with_map('', filekvsFullNames, opt);
+        
+        if strcmp(exportType, 'jsonp')
+            json_str = [callbackName '(' json_str ');'];
+        end;
+        
+        fid= fopen(exportPath, 'w');
+        fprintf(fid, '%s', json_str);
+        fclose(fid);
+end;
+
+
+
